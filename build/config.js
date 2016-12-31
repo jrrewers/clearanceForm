ccec816582a18b8c52df9e120cad09f75fdeec7c
Jarosław Rewers <jaroslaw.rewers@gmail.com>
2016-12-31 04:06:29 +0100

    deleted build

--- build/config.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by Jarek on 2016-05-26.
- */
-"use strict";
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImNvbmZpZy5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQSIsImZpbGUiOiJjb25maWcuanMiLCJzb3VyY2VSb290Ijoic3JjIiwic291cmNlc0NvbnRlbnQiOlsiLyoqXHJcbiAqIENyZWF0ZWQgYnkgSmFyZWsgb24gMjAxNi0wNS0yNi5cclxuICovXHJcbiJdfQ==
